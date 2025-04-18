d12097266852a2199f7a5a07d639121a138a42cc
MichalBr <br.misak@gmail.com>
2025-04-18 19:24:10 +0200

    v1.31 - Loading changes
    - Small color change

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -40,7 +40,7 @@ export const LoadingScreen = ({ onComplete }) => {
       </div>
 
       <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
-        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+        <div className="w-[40%] h-full bg-blue-600 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>
   );
