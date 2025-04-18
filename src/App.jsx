f93bf02e9dde258637d4a8c9df69cb0fb7c1f71e
MichalBr <br.misak@gmail.com>
2025-04-18 20:37:50 +0200

    v1.1 - Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
 import { Contact } from "./components/sections/Contact";
+import Footer from "./components/sections/Footer";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -29,6 +30,7 @@ function App() {
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </div>
     </>
   );
