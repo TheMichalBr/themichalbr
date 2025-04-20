9cbc8ced6fb415df4642d5b64cc5bd9186967aef
MichalBr <br.misak@gmail.com>
2025-04-20 12:03:34 +0200

    v1.31 - Video added

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,58 +1,28 @@
 import { RevealOnScroll } from "../RevealOnScroll";
 
 export const About = () => {
-  const Hobbies = [
-    "Game",
-    "Content creation",
-    "Game development",
-    "Games",
-    "Creating content",
-    "Editing",
-    "Programing",
-    "Music",
-    "Films",
-    "Collecting",
-    "Exploring",
-    "Technology",
-    "Memes",
-  ];
-
-  const ProgLan = ["C#", "C++", "HTML", "CSS", "Java", "JavaScript", "React", "Python", "SQL", "Godot", "Unity", "Source", "Unreal",];
+  const Hob = ["Games", "Programing", "Content creation", "Editing", "Development", "Music", "Films", "Chess", "Collecting", "Exploring", "Technology", "Memes"];
+  const Prog = ["C#", "C++", "HTML", "CSS", "JavaScript", "React", "Node.js", "Java", "Python", "SQL", "Godot", "Unity", "Source", "Unreal", "Adobe Photshop / Premiere"];
 
   return (
-    <section
-      id="aboutme"
-      className="min-h-screen flex items-center justify-center py-20"
-    >
+    <section id="aboutme" className="min-h-screen flex items-center justify-center py-20">
       <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {" "}
             About Me
           </h2>
 
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6">
-            My name is Michal, 23 years old and I've come from the Czech Republic – live in Brno.
-
-I can speak well in Czech, English and bit of German and Spanish (and other similar Slavic languages).
-
-I successfully finished studying Information technology in High school and English language school.
-
-Now I studying Information Technologies at the PEF Faculty of Mendel University and trying to survive it.
-
-My hobbies are games, technology, programing, creating content, editing, music, films, collecting, chess, exploring, memes and much more.
-
-I'm not going to write everything here, but if you have any questions, feel free to just message me @MichalBr on Discord or on my e-mail biz.michalbr@gmail.com.
-              Passionate developer with expertise in building scalable web
-              applications and creating innovative solutions.
+              My name is Michal, 23 years old and I've come from the Czech Republic – live in Brno. I can speak well in Czech, English and bit of German and Spanish (and other similar Slavic languages).
+              Passionate gamer and developer with expertise in building scalable web applications and creating innovative solutions.
             </p>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> My Hobbies</h3>
+                <h3 className="text-xl font-bold mb-4"> Hobbies</h3>
                 <div className="flex flex-wrap gap-2">
-                  {Hobbies.map((tech, key) => (
+                  {Hob.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -68,7 +38,7 @@ I'm not going to write everything here, but if you have any questions, feel free
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
                 <h3 className="text-xl font-bold mb-4"> Experience in</h3>
                 <div className="flex flex-wrap gap-2">
-                  {ProgLan.map((tech, key) => (
+                  {Prog.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -83,12 +53,26 @@ I'm not going to write everything here, but if you have any questions, feel free
             </div>
           </div>
 
+          <div className="rounded-xl p-4 border-white/10 border hover:-translate-y-1 transition-all mt-8 py-8">
+          <div className="max-w-6xl mx-auto px-4">
+                <div className="relative w-full max-w-4xl mx-auto" style={{ paddingTop: "56.25%" }}>
+                    <iframe
+                        className="absolute top-0 left-0 w-full h-full rounded-xl shadow-lg border border-white/10"
+                        src="https://www.youtube.com/embed/VgQDsqOi2wo"
+                        title="MichalBr - YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                </div>
+            </div>
+          </div>
+
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
                 <li>
-                  <strong>Information Technologies </strong> - Mendel University
+                  <strong>Information Technologies </strong> - at the PEF Faculty of Mendel University
                   (2024-Present)
                 </li>
                 <li>
