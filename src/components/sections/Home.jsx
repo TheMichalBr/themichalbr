eede965374d6a4eed19df75debebbea81b429e0c
MichalBr <br.misak@gmail.com>
2025-04-21 17:06:58 +0200

    1.21 - Fixed buttons href

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
           </p>
           <div className="flex justify-center space-x-4">
             <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">View Projects</a>
-            <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">Contact Me</a>
+            <a href="#projects" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">Contact Me</a>
           </div>
         </div>
       </RevealOnScroll>
