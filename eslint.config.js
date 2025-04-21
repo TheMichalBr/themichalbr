8d3be2743f74560f733a023d92536a16c5d0366c
MichalBr <br.misak@gmail.com>
2025-04-21 19:11:56 +0200

    v1.02 - Small change

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from 'eslint-plugin-react-hooks'
+import globals from 'globals'
+import js from '@eslint/js'
 
 export default [
   { ignores: ['dist'] },
