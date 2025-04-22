85b3f5ba612fae8dfe0f1fe165dc75fd0411a695
MichalBr <br.misak@gmail.com>
2025-04-22 21:21:14 +0200

    v1.24 - Small change

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,10 +5,7 @@ export const Home = () => {
     <section id="home" className="min-h-screen flex items-center justify-center relative">
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
-            Hi, I'm MichalBr
-          </h1>
-
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right"> Hi, I'm MichalBr </h1>
           <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
             I’m a content creator, gamer and developer who loves crafting clean, scalable web applications. My goal is to build solutions that offer both exceptional performance and a delightful user experience.
           </p>
