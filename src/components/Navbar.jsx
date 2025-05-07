acf4fea4508114585a32fb991cafe6425edbb866
MichalBr <br.misak@gmail.com>
2025-05-07 10:29:37 +0200

    v1.3 - Navbar change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Navbar = ({ menuOpen, setMenuOpen }) => {
+  const [activeSection, setActiveSection] = useState("");
+
   useEffect(() => {
     document.body.style.overflow = menuOpen ? "hidden" : "";
+
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section");
+      let current = "";
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop - 100;
+        if (window.scrollY >= sectionTop) {
+          current = section.getAttribute("id");
+        }
+      });
+      setActiveSection(current);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+
   }, [menuOpen]);
   return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg px-3.5 py-0.5">
@@ -15,10 +33,14 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
           </div>
 
           <div className="hidden md:flex items-center space-x-8">
-            <a href="#aboutme" className="text-gray-300 hover:text-white transition-colors hover:scale-102">About me</a>
-            <a href="#equipment" className="text-gray-300 hover:text-white transition-colors hover:scale-102">Equipment</a>
-            <a href="#games" className="text-gray-300 hover:text-white transition-colors hover:scale-102">Games</a>
-            <a href="#projects" className="text-gray-300 hover:text-white transition-colors hover:scale-102">Projects</a>
+            <a href="#aboutme" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 ${
+                activeSection === "aboutme" ? "font-bold" : ""}`}>About me</a>
+            <a href="#equipment" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 ${
+                activeSection === "equipment" ? "font-bold" : ""}`}>Equipment</a>
+            <a href="#games" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 ${
+                activeSection === "games" ? "font-bold" : ""}`}>Games</a>
+            <a href="#projects" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 ${
+                activeSection === "projects" ? "font-bold" : ""}`}>Projects</a>
           </div>
         </div>
       </div>
