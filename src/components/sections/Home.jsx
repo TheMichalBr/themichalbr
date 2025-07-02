da8b652e0002b050d9964f0fd3396b9de8282267
MichalBr <br.misak@gmail.com>
2025-07-02 21:59:59 +0200

    v1.35 - Removed matrix

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,10 +1,8 @@
 import { RevealOnScroll } from "../RevealOnScroll";
-import { MatrixRainBackground } from "./MatrixRainBackground"; // přidejte tento import
 
 export const Home = () => {
   return (
     <section id="home" className="min-h-screen flex items-center justify-center relative overflow-hidden">
-      <MatrixRainBackground /> {/* přidejte tuto řádku */}
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right"> Hi, I'm MichalBr </h1>
