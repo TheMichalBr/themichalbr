34935b6c3cea7552f0178f3a824d616da2988abb
MichalBr <br.misak@gmail.com>
2025-07-06 20:54:13 +0200

    v1.04 - Added plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite"
+import cleanPlugin from 'vite-plugin-clean';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), cleanPlugin()],
   base: "/themichalbr",
 })
