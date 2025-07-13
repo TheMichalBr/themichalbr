1146981fe7e414af51507e178bc1b2ce0e2ac20d
MichalBr <br.misak@gmail.com>
2025-07-13 10:49:24 +0200

    v1.59 - Animation improvements and polishing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,13 +20,18 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-
   }, [menuOpen]);
   return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg px-3.5 py-0.5">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-          <a href="#home" className="font-mono text-4xl text-white hover:scale-106 transition-colors duration-100" style={{ fontFamily: 'Bellibish' }}>M</a>
+          <a
+            href="#home"
+            className="font-mono text-4xl text-white hover:scale-106 transition-colors duration-100"
+            style={{ fontFamily: "Bellibish" }}
+          >
+            M
+          </a>
 
           <div
             className="w-7 h-5 relative cursor-pointer z-40 md:hidden flex flex-col justify-between group navbar-hamburger"
@@ -57,10 +62,30 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
             />
           </div>
           <div className="hidden md:flex items-center space-x-8">
-            <a href="#aboutme" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "aboutme" ? "font-bold" : ""}`}>About me</a>
-            <a href="#equipment" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "equipment" ? "font-bold" : ""}`}>Equipment</a>
-            <a href="#games" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "games" ? "font-bold" : ""}`}>Games</a>
-            <a href="#projects" className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "projects" ? "font-bold" : ""}`}>Projects</a>
+            <a
+              href="#aboutme"
+              className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "aboutme" ? "font-bold" : ""}`}
+            >
+              About me
+            </a>
+            <a
+              href="#equipment"
+              className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "equipment" ? "font-bold" : ""}`}
+            >
+              Equipment
+            </a>
+            <a
+              href="#games"
+              className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "games" ? "font-bold" : ""}`}
+            >
+              Games
+            </a>
+            <a
+              href="#projects"
+              className={`text-gray-300 hover:text-white transition-colors hover:scale-102 duration-300 ${activeSection === "projects" ? "font-bold" : ""}`}
+            >
+              Projects
+            </a>
           </div>
         </div>
       </div>
