9b5603a458594e0e5f35df88c0004c86af199879
MichalBr <br.misak@gmail.com>
2025-07-24 19:17:02 +0200

    v1.79.3 - Small adjustments

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -284,7 +284,7 @@ export const Projects = () => {
           {" "}
           {/* max-w-5xl ?*/}
           <div className="text-center mb-10">
-            <h2 className="text-3xl font-bold mb-4 bg-gradient-to-r from-blue-500 via-sky-600 to-cyan-400 bg-clip-text text-transparent select-none">
+            <h2 className="text-4xl font-bold mb-4 bg-gradient-to-r from-blue-500 via-sky-600 to-cyan-400 bg-clip-text text-transparent select-none">
               Featured projects
             </h2>
             <p className="text-gray-400 text-lg max-w-2xl mx-auto select-none">
