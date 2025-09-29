9e869f5f689539d74ac44b21bc8423104805c821
MichalBr <br.misak@gmail.com>
2025-09-29 09:59:12 +0200

    v1.65 - Small fix and section removed
    Watched section moved into game section.

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,12 @@ import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Equipment } from "./components/sections/Equipment";
 import { Games } from "./components/sections/Games";
-import Watched from "./components/sections/Watched";
 import { Projects } from "./components/sections/Projects";
 import Footer from "./components/sections/Footer";
 
 import "./index.css";
 import "./App.scss";
-import './assets/fonts/fonts.css';
+import "./assets/fonts/fonts.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -21,14 +20,15 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
-      <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}
+      >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
         <About />
         <Equipment />
         <Games />
-        <Watched />
         <Projects />
         <Footer />
       </div>
