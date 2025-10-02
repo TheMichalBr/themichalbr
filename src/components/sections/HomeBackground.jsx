e349efd378f449a776752b28bd1cd6019dd6609e
MichalBr <br.misak@gmail.com>
2025-10-02 21:22:03 +0200

    v1.26 - Small change and moved

--- src/components/sections/HomeBackground.jsx
+++ src/components/sections/HomeBackground.jsx
@@ -94,7 +94,7 @@ const SilkPlane = forwardRef(function SilkPlane({ uniforms }, ref) {
 });
 SilkPlane.displayName = "SilkPlane";
 
-const HomeBackground = ({
+const HomeBackGround = ({
     speed = 5,
     scale = 1,
     color = "#7B7481",
@@ -134,4 +134,4 @@ const HomeBackground = ({
     );
 };
 
-export default HomeBackground;
+export default HomeBackGround;
