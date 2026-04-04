40fb46b4fa4d60cca4e647274007fcc23e4e95ee
MichalBr <br.misak@gmail.com>
2026-04-04 14:41:15 +0200

    v1.1 - ESLINT config changed

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,47 @@
 import globals from 'globals'
 import js from '@eslint/js'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
 export default [
-  { ignores: ['dist'] },
+  { 
+    ignores: ['dist', 'node_modules'] 
+  },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
       parserOptions: {
-        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
+        ecmaVersion: 'latest',
+        project: './tsconfig.app.json',
         sourceType: 'module',
       },
+      sourceType: 'module',
     },
     plugins: {
+      '@typescript-eslint': tsPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      ...tsPlugin.configs.recommended.rules,
+
+      '@typescript-eslint/no-unused-vars': [
+        'error', 
+        { 
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^[A-Z_]' 
+        }
+      ],
+      'no-fallthrough': 'error',
+      'no-unused-vars': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
