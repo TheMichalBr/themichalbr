d756399c11bded0963dd66c867e13aa9b8ebbe1c
MichalBr <br.misak@gmail.com>
2026-04-04 13:35:04 +0200

    v1.06 - Transition to TS

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import react from '@vitejs/plugin-react';
-import tailwindcss from "@tailwindcss/vite"
-import cleanPlugin from 'vite-plugin-clean';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [react(), tailwindcss(), cleanPlugin()],
-  base: "/themichalbr",
-})
