5cb016bff01fd43f885bf5b9be79281e80e4a6ff
MichalBr <br.misak@gmail.com>
2026-04-06 10:52:24 +0200

    v1.3.4 - Data list updated

--- src/data/GamesData.ts
+++ src/data/GamesData.ts
@@ -20,7 +20,7 @@ export const reviews: Review[] = [
     title: "Harry Potter and the Philosopher's Stone",
     image: "https://miro.medium.com/1*XdoGGruC9N0bKdWpAIFdjw.jpeg",
     tag: "film",
-    rating: "9/10",
+    rating: "9.0",
     info: {
       year: "2001",
       genre: "Fantasy",
@@ -30,7 +30,7 @@ export const reviews: Review[] = [
     title: "Blue Protocol: Star Resonance",
     image: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/3681810/bab705e2414243cd247a14a07b2d5490dcd2cefd/header.jpg?t=1760243884",
     tag: "game",
-    rating: "6.5/10",
+    rating: "6.5",
     info: {
       year: "2025",
       genre: "MMORPG",
@@ -41,7 +41,7 @@ export const reviews: Review[] = [
     title: "The SpongeBob SquarePants Movie",
     image: "",
     tag: "film",
-    rating: "7.5/10",
+    rating: "7.5",
     info: {
       year: "2004",
       genre: "Animated",
@@ -51,7 +51,7 @@ export const reviews: Review[] = [
     title: "Cyberpunk 2077",
     image: "",
     tag: "game",
-    rating: "10/10",
+    rating: "10",
     info: {
       year: "2020",
       genre: "RPG",
@@ -63,7 +63,7 @@ export const reviews: Review[] = [
     title: "SpongeBob SquarePants",
     image: "",
     tag: "series",
-    rating: "9.0/10",
+    rating: "9.0",
     info: {
       year: "1999",
       genre: "Animated",
@@ -73,7 +73,7 @@ export const reviews: Review[] = [
     title: "Comeback",
     image: "",
     tag: "series",
-    rating: "9.5/10",
+    rating: "9.5",
     info: {
       year: "2008",
       genre: "Comedy",
@@ -83,7 +83,7 @@ export const reviews: Review[] = [
     title: "Doom Eternal",
     image: "",
     tag: "game",
-    rating: "9.2/10",
+    rating: "9.2",
     info: {
       year: "2020",
       genre: "FPS",
@@ -96,7 +96,7 @@ export const reviews: Review[] = [
     title: "The Simpsons Movie",
     image: "",
     tag: "film",
-    rating: "9.0/10",
+    rating: "9.0",
     info: {
       year: "2007",
       genre: "Animated",
@@ -106,7 +106,7 @@ export const reviews: Review[] = [
     title: "The Pink Panther 2",
     image: "",
     tag: "film",
-    rating: "7.0/10",
+    rating: "7.0",
     info: {
       year: "2009",
       genre: "Comedy",
@@ -116,7 +116,7 @@ export const reviews: Review[] = [
     title: "The Pink Panther",
     image: "",
     tag: "film",
-    rating: "7.5/10",
+    rating: "7.5",
     info: {
       year: "2006",
       genre: "Comedy",
@@ -126,7 +126,7 @@ export const reviews: Review[] = [
     title: "Police Academy 7: Mission to Moscow",
     image: "",
     tag: "film",
-    rating: "6.0/10",
+    rating: "6.0",
     info: {
       year: "1994",
       genre: "Comedy",
@@ -136,7 +136,7 @@ export const reviews: Review[] = [
     title: "Police Academy 6: City Under Siege",
     image: "",
     tag: "film",
-    rating: "6.7/10",
+    rating: "6.7",
     info: {
       year: "1989",
       genre: "Comedy",
@@ -146,7 +146,7 @@ export const reviews: Review[] = [
     title: "Police Academy 5: Assignment: Miami Beach",
     image: "",
     tag: "film",
-    rating: "6.9/10",
+    rating: "6.9",
     info: {
       year: "1988",
       genre: "Comedy",
@@ -156,7 +156,7 @@ export const reviews: Review[] = [
     title: "Police Academy 4: Citizens on Patrol",
     image: "",
     tag: "film",
-    rating: "7.0/10",
+    rating: "7.0",
     info: {
       year: "1987",
       genre: "Comedy",
@@ -166,7 +166,7 @@ export const reviews: Review[] = [
     title: "Police Academy 3: Back in Training",
     image: "",
     tag: "film",
-    rating: "7.2/10",
+    rating: "7.2",
     info: {
       year: "1986",
       genre: "Comedy",
@@ -176,7 +176,7 @@ export const reviews: Review[] = [
     title: "Police Academy 2: Their First Assignment",
     image: "",
     tag: "film",
-    rating: "7.4/10",
+    rating: "7.4",
     info: {
       year: "1985",
       genre: "Comedy",
@@ -186,7 +186,7 @@ export const reviews: Review[] = [
     title: "Police Academy",
     image: "",
     tag: "film",
-    rating: "7.5/10",
+    rating: "7.5",
     info: {
       year: "1984",
       genre: "Comedy",
