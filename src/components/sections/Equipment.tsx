a734a97124f7f040c7aadfa7e1ec3bf12a00952c
MichalBr <br.misak@gmail.com>
2026-06-27 19:38:09 +0200

    v1.88.13 - Small changes

--- src/components/sections/Equipment.tsx
+++ src/components/sections/Equipment.tsx
@@ -77,7 +77,7 @@ const equipmentData = [
     name: "MSI MPG X870E CARBON WIFI",
     image: mbI,
     specs: {
-      biosVersion: "7E49v1A95",
+      biosVersion: "7E49v1AA0",
       formFactor: "ATX",
       socket: "AM5",
       chipset: "X870E",
